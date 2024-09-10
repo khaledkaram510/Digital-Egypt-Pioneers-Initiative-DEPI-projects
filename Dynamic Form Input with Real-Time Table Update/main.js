b19c5242905d9b4a84aeef6a64bdc74eee7c4a51
khaledkaram510 <khaleddaib510@gmail.com>
2024-09-11 01:57:13 +0300

    m

--- Dynamic Form Input with Real-Time Table Update/main.js
+++ Dynamic Form Input with Real-Time Table Update/main.js
@@ -1,87 +1,70 @@
+let name = document.querySelector(".name");
+// console.log(name.children[1]);
+let email = document.querySelector(".email");
+let phone = document.querySelector(".phone");
+let curse = document.querySelector(".curseName");
+let tableBody = document.querySelector("#tableBody");
+let regName = /^[A-Z][a-z]+ [A-Z][a-z]+ [A-Z][a-z]+ [A-Z][a-z]+$/;
+let regEmail = /^\w+@\w+.\w+$/i;
+let regPhone = /^01[0-2 5]\d{8}$/;
+let regCurse = /^\w+$/;
+let boolName = false;
+let boolEmail = false;
+let boolPhone = false;
+let boolCurse = false;
+
+name.children[1].addEventListener("change", function () {
+  if (!regName.test(this.value)) {
+    name.children[0].style.color = "#ff4b20";
+  }else{
+    name.children[0].style.color = "#57b846";
+    boolName = true;
+  }
+});
+
+email.children[1].addEventListener("change", function () {
+  if (!regEmail.test(this.value)) {
+    email.children[0].style.color = "#ff4b20";
+  }else{
+    email.children[0].style.color = "#57b846";
+    boolEmail = true;
+  }
+});
+
+phone.children[1].addEventListener("change", function () {
+  if (!regPhone.test(this.value)) {
+    phone.children[0].style.color = "#ff4b20";
+  }else{
+    phone.children[0].style.color = "#57b846";
+    boolPhone = true;
+  }
+});
+
+curse.children[1].addEventListener("change", function () {
+  if (!regCurse.test(this.value)) {
+    curse.children[0].style.color = "#ff4b20";
+  }else{
+    curse.children[0].style.color = "#57b846";
+    boolCurse = true;
+  }
+});
+
+document.querySelector("form").addEventListener("submit", function (e) {
+  e.preventDefault();
+  if(boolName && boolEmail && boolPhone && boolCurse){
+    tableBody.innerHTML += `
+      <tr>
+        <td>${name.children[1].value}</td>
+        <td>${email.children[1].value}</td>
+        <td>${phone.children[1].value}</td>
+        <td>${curse.children[1].value}</td>
+      </tr>
+    `;
+    document.querySelector("#reset").click();
+  }
+});
 
 
-// let prss = new Promise((res,reg)=>{
-//     let req =  new XMLHttpRequest();
-//     // console.log(req);
-//     req.onload=()=>{
-//         // console.log(req)
-//         if(req.readyState === 4 ){
-//             if(req.status === 200){
-//                 res(JSON.parse(req.responseText));
-//             }else{
-//                 reg("error in requist");
-//             }
-//             // console.log(JSON.parse(req.responseText));
-//         }else{
-//             reg("error in requist");
-//         }
-//     }
-//     // req.open("GET", "https://dummyjson.com/products",true);
-//     req.open("GET", "https://reqres.in/api/users?page=2",true);
-//     // req.open("GET", "https://reqres.in/api/unknown/23",true);
-//     req.send();
-
-// });
-// prss.then((res)=>{
-//     let newr;
-//     res.data.forEach(element => {
-//     //    console.log(element.email) 
-//     document.write(`<h3>${element.first_name} ${element.first_name}</h3>`);
-//     document.write(`<h5>${element.email}</h5>`);
-    
-//     });
-//     newr = res.data.map(ele=>{
-//         return ele.id;
-//     })
-//     newr.forEach((e)=>{
-//         console.log(e);
-//     });
-// },(reg)=>{
-//     console.log(reg);
-// }
-// ).catch((reg)=>{
-//     console.log(reg)
-// })
 
 
 
-// async function ffn(){
-//     let res = await fetch("https://reqres.in/api/users?page=2");
-//     let data  = await res.json();
-//     return data.data;
-// }
-// ffn().then((res)=>{
-//     console.log(res);
-//     res.forEach(element => {
-//         console.log(element.first_name)
-//     });
-// }).catch((reg)=>{
-//     // console.log("khaled");
-// });
-// $()
-// console.log($("h1"))
-// $.ajax({
-//     type: "GET",
-//     url: "https://reqres.in/api/users?page=2",
-//     success: function (response) {
-//         console.log(response);
-//     }
-// });
-// $(selector).data({
-//     key: value
-    
-// });
-// $("p").attr("class", function (index, attr) { 
-//      console.log(attr);
-//      console.log(index);
-// });
-
-// ##### copy button #####
-// let button =document.querySelector("button");
-
-// console.log(document.querySelector("P"));
-// button.onclick = ()=>{
-//     navigator.clipboard.writeText(document.querySelector("P").innerHTML);
-//     alert("text copyied");
-//     // console.log(document.querySelector("P").innerHTML);
-// };
