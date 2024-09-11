b75961ea0b9f49b92b108884d895d9b71e49d4b7
khaledkaram510 <khaleddaib510@gmail.com>
2024-09-11 13:58:17 +0300

    m

--- Dynamic Eye Animation with Amatrus Effect/main.js
+++ Dynamic Eye Animation with Amatrus Effect/main.js
@@ -1,14 +1,11 @@
 const move1 = document.getElementById("move1");
 const move2 = document.getElementById("move2");
 let timeoutId;
-const originPosition = { top: '50%', left: '50%' }; // Define the original position
+const originPosition = { top: '50%', left: '50%' };
 
 document.body.onpointermove = event => {
     const { clientX, clientY } = event;
 
-    // console.log(Math.round(clientX / 10), Math.round(clientY / 10));
-
-    // Limit the range of movement by scaling down the impact of clientX and clientY
     const movementFactor = 2; // Adjust this value to control the movement range (smaller = less movement)
 
     const limitedTop = 50 + (clientY / window.innerHeight - 0.5) * 20 * movementFactor;
@@ -24,10 +21,8 @@ document.body.onpointermove = event => {
         left: `${limitedLeft}%`
     }, { duration: 500, fill: "forwards" });
 
-    // Clear the existing timeout to reset the timer
     clearTimeout(timeoutId);
 
-    // Set a new timeout to move the element back to the original position after 3 seconds of no movement
     timeoutId = setTimeout(() => {
         move1.animate({
             top: originPosition.top,
