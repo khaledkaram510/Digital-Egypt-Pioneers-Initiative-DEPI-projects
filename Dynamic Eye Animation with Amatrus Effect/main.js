4fabe59a3739cd886983fd7591a0948a065fb438
khaledkaram510 <khaleddaib510@gmail.com>
2024-09-11 13:57:40 +0300

    m

--- Dynamic Eye Animation with Amatrus Effect/main.js
+++ Dynamic Eye Animation with Amatrus Effect/main.js
@@ -1,87 +1,41 @@
+const move1 = document.getElementById("move1");
+const move2 = document.getElementById("move2");
+let timeoutId;
+const originPosition = { top: '50%', left: '50%' }; // Define the original position
 
+document.body.onpointermove = event => {
+    const { clientX, clientY } = event;
 
-// let prss = new Promise((res,reg)=>{
-//     let req =  new XMLHttpRequest();
-//     // console.log(req);
-//     req.onload=()=>{
-//         // console.log(req)
-//         if(req.readyState === 4 ){
-//             if(req.status === 200){
-//                 res(JSON.parse(req.responseText));
-//             }else{
-//                 reg("error in requist");
-//             }
-//             // console.log(JSON.parse(req.responseText));
-//         }else{
-//             reg("error in requist");
-//         }
-//     }
-//     // req.open("GET", "https://dummyjson.com/products",true);
-//     req.open("GET", "https://reqres.in/api/users?page=2",true);
-//     // req.open("GET", "https://reqres.in/api/unknown/23",true);
-//     req.send();
+    // console.log(Math.round(clientX / 10), Math.round(clientY / 10));
 
-// });
-// prss.then((res)=>{
-//     let newr;
-//     res.data.forEach(element => {
-//     //    console.log(element.email) 
-//     document.write(`<h3>${element.first_name} ${element.first_name}</h3>`);
-//     document.write(`<h5>${element.email}</h5>`);
-    
-//     });
-//     newr = res.data.map(ele=>{
-//         return ele.id;
-//     })
-//     newr.forEach((e)=>{
-//         console.log(e);
-//     });
-// },(reg)=>{
-//     console.log(reg);
-// }
-// ).catch((reg)=>{
-//     console.log(reg)
-// })
+    // Limit the range of movement by scaling down the impact of clientX and clientY
+    const movementFactor = 2; // Adjust this value to control the movement range (smaller = less movement)
 
+    const limitedTop = 50 + (clientY / window.innerHeight - 0.5) * 20 * movementFactor;
+    const limitedLeft = 50 + (clientX / window.innerWidth - 0.5) * 20 * movementFactor;
 
+    // Move the element based on the mouse position within the limited range
+    move1.animate({
+        top: `${limitedTop}%`,
+        left: `${limitedLeft}%`
+    }, { duration: 500, fill: "forwards" });
+    move2.animate({
+        top: `${limitedTop}%`,
+        left: `${limitedLeft}%`
+    }, { duration: 500, fill: "forwards" });
 
-// async function ffn(){
-//     let res = await fetch("https://reqres.in/api/users?page=2");
-//     let data  = await res.json();
-//     return data.data;
-// }
-// ffn().then((res)=>{
-//     console.log(res);
-//     res.forEach(element => {
-//         console.log(element.first_name)
-//     });
-// }).catch((reg)=>{
-//     // console.log("khaled");
-// });
-// $()
-// console.log($("h1"))
-// $.ajax({
-//     type: "GET",
-//     url: "https://reqres.in/api/users?page=2",
-//     success: function (response) {
-//         console.log(response);
-//     }
-// });
-// $(selector).data({
-//     key: value
-    
-// });
-// $("p").attr("class", function (index, attr) { 
-//      console.log(attr);
-//      console.log(index);
-// });
+    // Clear the existing timeout to reset the timer
+    clearTimeout(timeoutId);
 
-// ##### copy button #####
-// let button =document.querySelector("button");
-
-// console.log(document.querySelector("P"));
-// button.onclick = ()=>{
-//     navigator.clipboard.writeText(document.querySelector("P").innerHTML);
-//     alert("text copyied");
-//     // console.log(document.querySelector("P").innerHTML);
-// };
+    // Set a new timeout to move the element back to the original position after 3 seconds of no movement
+    timeoutId = setTimeout(() => {
+        move1.animate({
+            top: originPosition.top,
+            left: originPosition.left
+        }, { duration: 1000, fill: "forwards" });
+        move2.animate({
+            top: originPosition.top,
+            left: originPosition.left
+        }, { duration: 1000, fill: "forwards" });
+    }, 3000); // 3 seconds
+};
